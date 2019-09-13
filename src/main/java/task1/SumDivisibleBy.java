0380be605cab85cb90451653dfb85540a814c4e5
Sergey <s.ivonin@urait.ru>
2019-09-13 14:28:15 +0300

    task 1

--- src/main/java/task1/SumDivisibleBy.java
+++ src/main/java/task1/SumDivisibleBy.java
@@ -11,14 +11,7 @@ public class SumDivisibleBy {
         this.number = number;
     }
 
-    /**
-     * Задача 1
-     * Числа, кратные 3 или 5
-     * Если выписать все натуральные числа меньше 10, кратные 3 или 5, то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
-     * Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
-     */
-
-    public int sumDivisibleBy() {
+    public int print() {
         int sum = 0;
         for (int i = 0; i < LIMIT_NUMBER; i++) {
             if (i % number == 0)
--- src/main/java/task1/Task1Result.java
+++ src/main/java/task1/Task1Result.java
@@ -2,10 +2,16 @@ package task1;
 
 public class Task1Result {
 
+    /**
+     * Задача 1
+     * Числа, кратные 3 или 5
+     * Если выписать все натуральные числа меньше 10, кратные 3 или 5, то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
+     * Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
+     */
+
     public static void main(String[] args) {
-        SumDivisibleBy sumDivisibleBy3 = new SumDivisibleBy(3);
-        SumDivisibleBy sumDivisibleBy5 = new SumDivisibleBy(5);
-        System.out.println(sumDivisibleBy3.sumDivisibleBy() + sumDivisibleBy5.sumDivisibleBy());
+
+        System.out.println(new SumDivisibleBy(3).print() + new SumDivisibleBy(5).print());
 
     }
 }

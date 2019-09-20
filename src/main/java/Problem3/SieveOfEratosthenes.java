7c97e4369fe7d0d55cbc5a1532f8506f45862fe2
ivonin85 <ivonin85@gmail.com>
2019-09-20 13:04:46 +0300

    Sieve of Eratosthenes

--- src/main/java/Problem3/SieveOfEratosthenes.java
+++ src/main/java/Problem3/SieveOfEratosthenes.java
@@ -6,11 +6,12 @@ import java.util.List;
 
 public class SieveOfEratosthenes {
 
-    static void higherNumber() {
+
+    static void higherNumber(long number) {
         List<Integer> lastNum = new ArrayList<>();
+
         final int N = 10000;
         int[] sieve = new int[N];
-
 
         for (int i = 2; i * i < N; ++i)
             if (sieve[i] == 0)
@@ -21,7 +22,7 @@ public class SieveOfEratosthenes {
 
         for (int i = 2; i < N; i++) {
             if (sieve[i] == 0)
-                if (600851475143L % i == 0) {
+                if (number % i == 0) {
                     lastNum.add(i);
                 }
 
@@ -31,8 +32,16 @@ public class SieveOfEratosthenes {
 
     }
 
+    /**
+     * Largest prime factor
+     * Problem 3
+     * The prime factors of 13195 are 5, 7, 13 and 29.
+     * What is the largest prime factor of the number 600851475143 ?
+     */
+
     public static void main(String[] args) {
-        higherNumber();
+        long number = 600851475143L;
+        higherNumber(number);
     }
 
 }
